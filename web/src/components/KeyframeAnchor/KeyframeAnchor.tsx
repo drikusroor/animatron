109712452a372ff24695517e6e81dcf3888cf0b3
Drikus Roor <drikusroor@gmail.com>
2023-08-10 18:06:18 +0200

    feat: Make roundedness optional

--- web/src/components/KeyframeAnchor/KeyframeAnchor.tsx
+++ web/src/components/KeyframeAnchor/KeyframeAnchor.tsx
@@ -2,11 +2,16 @@ import { IKeyframe } from '../../types/keyframe.interface'
 
 interface KeyframeAnchorProps {
   keyframe: IKeyframe
+  rounded: boolean
 }
 
-const KeyframeAnchor = (_props: KeyframeAnchorProps) => {
+const KeyframeAnchor = ({ rounded = true }: KeyframeAnchorProps) => {
   return (
-    <div className="absolute -left-2 top-2 h-4 w-4 rotate-45 cursor-pointer rounded-full border border-gray-400 bg-gray-300 hover:bg-gray-400"></div>
+    <div
+      className={`absolute -left-2 top-2 h-4 w-4 rotate-45 cursor-pointer border border-gray-400 bg-gray-300 hover:bg-gray-400 ${
+        rounded ? 'rounded-full' : ''
+      }`}
+    ></div>
   )
 }
 
