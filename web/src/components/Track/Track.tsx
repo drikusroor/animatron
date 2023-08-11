bbf4919a9f5758e77b2e3f6e5ddc7b1328f3150b
Drikus Roor <drikusroor@gmail.com>
2023-08-11 09:58:22 +0200

    style: Style a little block left of the track

--- web/src/components/Track/Track.tsx
+++ web/src/components/Track/Track.tsx
@@ -10,15 +10,13 @@ const Track = ({ track }: ITrackProps) => {
   const bgColor = track.color ? { backgroundColor: track.color } : {}
 
   return (
-    <div
-      style={{ ...bgColor }}
-      className="m-2 flex items-center bg-slate-700 py-3"
-    >
-      {track.clips.map((clip, index) => (
-        <div key={index} className="">
-          <Clip clip={clip} />
-        </div>
-      ))}
+    <div style={{ ...bgColor }} className="m-2 flex items-center bg-slate-700">
+      <div className="w-6"></div>
+      <div className="flex items-center border-l border-slate-400 py-3">
+        {track.clips.map((clip, index) => (
+          <Clip key={index} clip={clip} />
+        ))}
+      </div>
     </div>
   )
 }
