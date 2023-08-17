4b8196e03bd6d08d96de24528c084851cb27710a
Drikus Roor <drikusroor@gmail.com>
2023-08-17 20:10:26 +0200

    feat: Add (non-functioning) save changes button

--- web/src/components/AnimationEditor/AnimationEditor.tsx
+++ web/src/components/AnimationEditor/AnimationEditor.tsx
@@ -39,9 +39,16 @@ const AnimationEditor = (props: AnimationEditorProps) => {
 
   return (
     <div className="flex h-screen flex-row">
-      <div className="flex h-screen w-64 flex-col border-e p-2">
-        <h2 className="text-sm font-bold uppercase">Entities</h2>
-        <EntityListContainer />
+      <div className="flex h-screen w-64 flex-col justify-between border-e p-2">
+        <div>
+          <h2 className="text-sm font-bold uppercase">Entities</h2>
+          <EntityListContainer />
+        </div>
+        <div>
+          <button className="w-full rounded-lg bg-amber-400 px-4 py-2 font-bold uppercase text-gray-800 transition-colors duration-200 ease-in-out hover:bg-amber-500">
+            Save changes
+          </button>
+        </div>
       </div>
       <div className="flex h-screen w-full flex-col justify-end border-e">
         <EntityEditorContainer />
