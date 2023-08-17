106af54e519074a3608882e915e9241b38c64556
Drikus Roor <drikusroor@gmail.com>
2023-08-17 12:12:38 +0200

    test: Update keyframe editor test with updated props

--- web/src/components/KeyframeEditor/KeyframeEditor.test.tsx
+++ web/src/components/KeyframeEditor/KeyframeEditor.test.tsx
@@ -1,5 +1,7 @@
 import { render } from '@redwoodjs/testing/web'
 
+import { createKeyframe } from 'src/__mocks__/mockData'
+
 import KeyframeEditor from './KeyframeEditor'
 
 //   Improve this test with help from the Redwood Testing Doc:
@@ -7,8 +9,18 @@ import KeyframeEditor from './KeyframeEditor'
 
 describe('KeyframeEditor', () => {
   it('renders successfully', () => {
+    const keyframe = createKeyframe()
+    const deselectKeyframe = jest.fn()
+    const updateKeyframe = jest.fn()
+
     expect(() => {
-      render(<KeyframeEditor />)
+      render(
+        <KeyframeEditor
+          keyframe={keyframe}
+          deselectKeyframe={deselectKeyframe}
+          updateKeyframe={updateKeyframe}
+        />
+      )
     }).not.toThrow()
   })
 })
