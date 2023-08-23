6671098c0bc970f5084ca9479f358c99f84e2fd6
Drikus Roor <drikusroor@gmail.com>
2023-08-23 16:42:11 +0200

    feat: Show 10 most recent animations

--- web/src/pages/FrontPage/FrontPage.tsx
+++ web/src/pages/FrontPage/FrontPage.tsx
@@ -1,19 +1,37 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+
+import RecentAnimationsCell from 'src/components/RecentAnimationsCell/RecentAnimationsCell'
 
 const FrontPage = () => {
   return (
     <>
       <MetaTags title="Front" description="Front page" />
 
-      <h1>FrontPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/FrontPage/FrontPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>front</code>, link to me with `
-        <Link to={routes.front()}>Front</Link>`
-      </p>
+      <div className="mx-auto max-w-4xl p-4">
+        <div className="grid gap-4 md:grid-cols-2">
+          <div>
+            <h1 className="text-2xl font-semibold">Welcome to Popi</h1>
+            <p className="mt-2">
+              Popi is a tool for creating animations using an intuitive
+              interface. It currently exports animations to CSS, but in the
+              future it will also export to SVG and GIF.
+            </p>
+
+            <a href="/app" className="mt-4 inline-block">
+              <button
+                className="rounded bg-slate-600 px-4 py-2 font-bold text-white hover:bg-slate-700 disabled:cursor-not-allowed disabled:opacity-50"
+                title="Start animating is not implemented yet"
+                aria-label="Start animating is not implemented yet"
+                disabled
+              >
+                Start animating
+              </button>
+            </a>
+          </div>
+
+          <RecentAnimationsCell />
+        </div>
+      </div>
     </>
   )
 }
