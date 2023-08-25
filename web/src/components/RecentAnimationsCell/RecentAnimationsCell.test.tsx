132dbe35d382e6bb831de7b0b0510017f6ae5605
Drikus Roor <drikusroor@gmail.com>
2023-08-25 10:15:53 +0200

    lint: Fix linting issues

--- web/src/components/RecentAnimationsCell/RecentAnimationsCell.test.tsx
+++ web/src/components/RecentAnimationsCell/RecentAnimationsCell.test.tsx
@@ -1,4 +1,5 @@
 import { render } from '@redwoodjs/testing/web'
+
 import { Loading, Empty, Failure, Success } from './RecentAnimationsCell'
 import { standard } from './RecentAnimationsCell.mock'
 
