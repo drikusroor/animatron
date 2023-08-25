0280babed0907365385e925a89e82e25ae8981d8
Drikus Roor <drikusroor@gmail.com>
2023-08-25 12:16:54 +0200

    feat: Integrate add new entity button

--- web/src/components/AddNewEntityButton/AddNewEntityButton.tsx
+++ web/src/components/AddNewEntityButton/AddNewEntityButton.tsx
@@ -1,8 +1,15 @@
 import { FaPlusCircle } from 'react-icons/fa'
 
-const AddNewEntityButton = () => {
+interface IAddNewEntityButtonProps {
+  onClick: () => void
+}
+
+const AddNewEntityButton = ({ onClick }: IAddNewEntityButtonProps) => {
   return (
-    <button className="flex w-full flex-row items-center gap-2 rounded-lg bg-amber-400 px-4 py-2 font-bold uppercase text-gray-800 transition-colors duration-200 ease-in-out hover:bg-amber-500">
+    <button
+      onClick={onClick}
+      className="flex w-full flex-row items-center gap-2 rounded-lg bg-amber-400 px-4 py-2 font-bold uppercase text-gray-800 transition-colors duration-200 ease-in-out hover:bg-amber-500"
+    >
       <FaPlusCircle />
       Add new entity
     </button>
