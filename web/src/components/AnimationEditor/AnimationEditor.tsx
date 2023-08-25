007afd320f4ec956203fb66515387d5e8c54e0d5
Drikus Roor <drikusroor@gmail.com>
2023-08-25 11:34:01 +0200

    feat: Add save icon to save changes button

--- web/src/components/AnimationEditor/AnimationEditor.tsx
+++ web/src/components/AnimationEditor/AnimationEditor.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 
+import { FaSave } from 'react-icons/fa'
+
 import { RWGqlError } from '@redwoodjs/forms'
 
 import { useBoundStore } from 'src/store'
@@ -51,9 +53,10 @@ const AnimationEditor = ({ onSave, tracks }: AnimationEditorProps) => {
         </div>
         <div>
           <button
-            className="w-full rounded-lg bg-amber-400 px-4 py-2 font-bold uppercase text-gray-800 transition-colors duration-200 ease-in-out hover:bg-amber-500"
+            className="flex w-full flex-row items-center gap-2 rounded-lg bg-amber-400 px-4 py-2 font-bold uppercase text-gray-800 transition-colors duration-200 ease-in-out hover:bg-amber-500"
             onClick={onSave}
           >
+            <FaSave />
             Save changes
           </button>
         </div>
