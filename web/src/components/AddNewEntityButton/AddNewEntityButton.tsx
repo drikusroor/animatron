cbd10ed29ec4839a2b4b23635af82a6efb766fc0
Drikus Roor <drikusroor@gmail.com>
2023-08-25 11:33:44 +0200

    refactor: Show icon first

--- web/src/components/AddNewEntityButton/AddNewEntityButton.tsx
+++ web/src/components/AddNewEntityButton/AddNewEntityButton.tsx
@@ -3,8 +3,8 @@ import { FaPlusCircle } from 'react-icons/fa'
 const AddNewEntityButton = () => {
   return (
     <button className="flex w-full flex-row items-center gap-2 rounded-lg bg-amber-400 px-4 py-2 font-bold uppercase text-gray-800 transition-colors duration-200 ease-in-out hover:bg-amber-500">
-      Add new entity
       <FaPlusCircle />
+      Add new entity
     </button>
   )
 }
