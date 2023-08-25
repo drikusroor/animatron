7d8756190ab55c94295cccdb130e3c4f6efec3f9
Drikus Roor <drikusroor@gmail.com>
2023-08-25 10:09:13 +0200

    fix(test): Supply correct mocking data to recent animations cell

--- web/src/components/RecentAnimationsCell/RecentAnimationsCell.mock.ts
+++ web/src/components/RecentAnimationsCell/RecentAnimationsCell.mock.ts
@@ -3,38 +3,17 @@ import { createAnimationAggregated, createEntity } from 'src/__mocks__/mockData'
 // Define your own mock data here:
 export const standard = (/* vars, { ctx, req } */) => ({
   recentAnimations: [
-    {
+    createAnimationAggregated({
       id: 42,
-      revisions: [
-        createAnimationAggregated({
-          entities: [createEntity(), createEntity()],
-        }),
-        createAnimationAggregated({
-          entities: [createEntity(), createEntity()],
-        }),
-      ],
-    },
-    {
+      entities: [createEntity(), createEntity()],
+    }),
+    createAnimationAggregated({
       id: 43,
-      revisions: [
-        createAnimationAggregated({
-          entities: [createEntity(), createEntity()],
-        }),
-        createAnimationAggregated({
-          entities: [createEntity(), createEntity()],
-        }),
-      ],
-    },
-    {
+      entities: [createEntity(), createEntity()],
+    }),
+    createAnimationAggregated({
       id: 44,
-      revisions: [
-        createAnimationAggregated({
-          entities: [createEntity(), createEntity()],
-        }),
-        createAnimationAggregated({
-          entities: [createEntity(), createEntity()],
-        }),
-      ],
-    },
+      entities: [createEntity(), createEntity()],
+    }),
   ],
 })
